7a233f80ce4f3058073bcb2e9962fff44fba8e2b
Pedro Henrique <pedrolima828@gmail.com>
2021-10-03 16:07:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function colorSite(){
+    document.body.classList.toggle('dark-page')
+}
+
+const colocar = {
+    add(){ 
+        document.querySelector('.page-land').classList.toggle('active')
+    },
+    close(){ 
+        document.querySelector('.page-land').classList.remove('active')
+    }
+}
+/*
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <link rel="stylesheet" href="./style.css">
+
+
+    
+</head>
+
+ */
